c5556a65da15dd8065638f40c8d58cc7fb5bc295
Kevin Villamizar <kvillamizar196@gmail.com>
2022-05-20 14:29:45 -0500

    Video component added | React player package added | Video stored in the project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/Header.js';
+import Video from './components/Video.js';
 
 function App() {
 	return (
 		<div className="App">
 			<Header />
+			<Video />
 		</div>
 	);
 }
---
+++ src/components/Video.js
@@ -0,0 +1,13 @@
+import React from "react";
+import '../styles/Video.css';
+
+function Video() {
+      return(
+            <div className="video-container">
+                  
+            </div>
+            
+      )
+}
+
+export default Video;
