d085db2966481acef0e1abbe54d1a306f59ce3d0
Kevin Villamizar <kvillamizar196@gmail.com>
2022-05-20 14:41:25 -0500

    Added firebase configuration file

---
+++ src/services/firebase_config.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBGD0R_olhFVw8WgTMhXOKyDiC7oMiIQOY",
+  authDomain: "siembra-producciones.firebaseapp.com",
+  projectId: "siembra-producciones",
+  storageBucket: "siembra-producciones.appspot.com",
+  messagingSenderId: "109860913160",
+  appId: "1:109860913160:web:a301754d02207e26fe11bd"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
