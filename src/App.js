36ad17a9f8591481241103e578ecf235c6d750c1
Kevin Villamizar <kvillamizar196@gmail.com>
2022-05-20 17:46:08 -0500

    Title component added

--- src/App.js
+++ src/App.js
@@ -2,14 +2,20 @@ import './App.css';
 import Header from './components/Header.js';
 import VideoCover from './components/VideoCover.js';
 import Video from './components/Video.js';
+import Title from './components/Title.js'
 
 function App() {
 	return (
 		<div className="App">
 			<Header />
 			<VideoCover />
-			<Video/>
-			<Video/>
+			<Title title='Realizaciones recientes' />
+			<Video />
+			<Title title='Próximamente' />
+			<Video />
+			<div className='green-title'>
+				<Title title='Síguenos' color='green' />
+			</div>
 		</div>
 	);
 }
