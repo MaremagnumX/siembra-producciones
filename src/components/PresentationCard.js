c69d960477dc97afa450579453d458f5b3fe822a
Kevin Villamizar <kvillamizar196@gmail.com>
2022-05-22 15:30:11 -0500

    Added presentation card component

--- src/components/PresentationCard.js
+++ src/components/PresentationCard.js
@@ -1,10 +1,18 @@
 import React from "react";
 import Image from '../components/Image.js';
 
-function PresentationCard (){
+function PresentationCard() {
       return (
-            <div className="presentation-container">
-                  <Image/>
+            <div className="info-container">
+                  <div className="photo-conatiner">
+                        <h1>Foto</h1>
+                  </div>
+                  <div className="name-conatiner">
+                        <h1>Name</h1>
+                  </div>
+                  <div className="charge-conatiner">
+                        <p>Charge</p>
+                  </div>
             </div>
       )
 }
