33e78fb97cf55cb52e749a04cc24f4a5717e60be
Kevin Villamizar <kvillamizar196@gmail.com>
2022-05-22 13:15:41 -0500

    Title 'El colectivo' added | Paragraph and style added

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,27 @@
 import React from "react";
+import Title from '../components/Title.js'
+import '../styles/About.css';
 
-function About () {
+function About() {
       return (
-            <div>
-                  <h1>About</h1>
+            <div className="container-about">
+                  <div className="green-title">
+                        <Title title='El colectivo' color='green' size='big' />
+                  </div>
+                  <div className="paragraph-container">
+                        <p className="paragraph">
+                        Lorem tempor amet laboris elit voluptate magna pariatur. 
+                        Commodo eu magna non consequat nulla enim sit pariatur veniam aliquip ipsum dolore sint. 
+                        Nisi nulla proident deserunt ullamco consectetur nostrud non.
+                        <br/>
+                        <br/>
+                        Lorem tempor amet laboris elit voluptate magna pariatur. 
+                        Commodo eu magna non consequat nulla enim sit pariatur veniam aliquip ipsum dolore sint. 
+                        Nisi nulla proident deserunt ullamco consectetur nostrud non. Lorem tempor amet laboris elit 
+                        voluptate magna pariatur. Commodo eu magna non consequat nulla enim sit pariatur veniam aliquip 
+                        ipsum dolore sint. Nisi nulla proident deserunt ullamco consectetur nostrud non.
+                        </p>
+                  </div>
             </div>
       )
 }
