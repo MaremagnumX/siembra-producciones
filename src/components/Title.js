847954b5affa83e0a9c10d317b25a02282897333
Kevin Villamizar <kvillamizar196@gmail.com>
2022-05-22 13:14:33 -0500

    Big title style adde to Title.css

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,10 +1,10 @@
 import React from "react";
 import '../styles/Title.css';
 
-function Title({title, color}) {
+function Title({title, color, size}) {
       return(
             <div className={color==='green' ? 'title-container' : ''}>
-                  <h1 className={color==='green' ? 'green-title' : 'purple-title'}>{title}</h1>
+                  <h1 className={(color==='green' ? (size==='big' ? 'green-title-big' : 'green-title') : 'purple-title')}>{title}</h1>
             </div>
       )
       
