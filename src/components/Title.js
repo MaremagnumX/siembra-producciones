2e7e0a09d4135af034bc7794ac01dfc09cb9da21
Kevin Villamizar <kvillamizar196@gmail.com>
2022-05-23 00:48:38 -0500

    Title section withbackground image added

--- src/components/Title.js
+++ src/components/Title.js
@@ -4,7 +4,7 @@ import '../styles/Title.css';
 function Title({title, color, size}) {
       return(
             <div className={color==='green' ? 'title-container' : ''}>
-                  <h1 className={(color==='green' ? (size==='big' ? 'green-title-big' : 'green-title') : 'purple-title')}>{title}</h1>
+                  <h1 className={(color==='green' ? (size==='big' ? 'green-title-big' : 'green-title') : (size==='big' ? 'purple-title-big' : 'purple-title'))}>{title}</h1>
             </div>
       )
       
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,9 +1,16 @@
 import React from "react";
+import '../styles/Contact.css';
+import Title from "../components/Title";
 
-function Contact () {
+function Contact() {
       return (
-            <div>
-                  <h1>Contact</h1>
+            <div className="contact-container">
+                  <div className="contact-cover">
+                        <div className="contact-filter"></div>
+                        <div className="contact-title-container">
+                              <Title className="contact-title" title="Escríbenos" size="big" />
+                        </div>
+                  </div>
             </div>
       )
 }
