405a5e8f7a57a73ee92275ce77e72081e31cebc3
Kevin Villamizar <kvillamizar196@gmail.com>
2022-05-23 01:47:42 -0500

    Added contact information section

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -11,6 +11,27 @@ function Contact() {
                               <Title className="contact-title" title="Escríbenos" size="big" />
                         </div>
                   </div>
+                  <div className="contact-data">
+                        <div className="contact-subtitle">
+                              <Title title={'Contacto'} color={'green'} />
+                        </div>
+                        <div className="contact-info">
+                              <div className="contact-info-text">
+                                    <p>
+                                          siembraproducciones@gmail.com<br /><br />
+                                          +57 318 399 2225<br /><br />
+                                          Horario: 8:30 - 1:00 | 2:30 - 6:30 Lunes a Sábado<br /><br />
+                                          Antonia Santos, Bucaramanga Santander
+                                    </p>
+                              </div>
+                        </div>
+                  </div>
+                  <div className="contact-map">
+
+                  </div>
+                  <div className="contact-form">
+
+                  </div>
             </div>
       )
 }
